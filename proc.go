d54bb00803b4b4c2e47752a2ca48be625a2bf732
Adam Haymond <coastgrd@gmail.com>
2020-01-24 16:55:01 -0700

    updating internal package names

--- proc.go
+++ proc.go
@@ -21,8 +21,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/prometheus/procfs/internal/fs"
-	"github.com/prometheus/procfs/internal/util"
+	"github.com/NodeSpy/procfs/internal/fs"
+	"github.com/NodeSpy/procfs/internal/util"
 )
 
 // Proc provides information about a running process.
